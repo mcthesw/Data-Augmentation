023432d4ad40ab9425a0dfa4fff51b1517684a3c
Sworld <1628767411@qq.com>
2022-01-16 21:28:28 +0800

    完善Patch类，修改主函数 目前存在问题，不能使用

--- DataObj.py
+++ DataObj.py
@@ -1,4 +1,5 @@
 import os
+import random
 from os import path
 from random import randint
 
@@ -18,7 +19,8 @@ class ImageData:
         return cls(file_name[:-4], image, masks)
 
     def __init__(self, file_name: str, image: numpy.ndarray, mask_polygons: dict):
-        self.mask_image = None
+        # noinspection PyTypeChecker
+        self.mask_image: dict = None
         self.name = file_name  # 这个是用于保存的ID
         self.image = image
         self.mask_polygons = mask_polygons
@@ -63,17 +65,47 @@ class ImageData:
 
 
 class Patch:
+
+    @classmethod
+    def create_from_image_data(cls, data: ImageData, patch_size: tuple = (128, 128)) -> list:
+        # 因为生成patch时需要mask，所以不能为空
+        assert data.mask_image is not None
+        results = []
+        # 切开原始图像
+        patch_images = split_img(data.image, patch_size)
+        # 把原始mask图片也切开
+        patch_masks = dict()
+        for mask_type in data.types:
+            patch_masks[mask_type] = list()
+            for origin_mask_image in data.mask_image[mask_type]:
+                patch_masks[mask_type].append(split_mask(origin_mask_image, patch_size))
+        # 按顺序进行组合，放入Patch对象
+        for i in range(len(patch_images)):
+            cur_patch_image = patch_images[i]
+            cur_patch_masks = dict()
+            for mask_type in data.types:
+                cur_patch_masks[mask_type] = list()
+                for mask in patch_masks[mask_type]:
+                    cur_patch_masks[mask_type].append(mask[i])
+            results.append(cls(cur_patch_image, cur_patch_masks))
+        return results
+
     def __init__(self, image: numpy.ndarray, mask_images: dict):
         self.image = image
-        self.shape = image.shape
+        self.shape = image.shape[0:2]
+        self.types = mask_images.keys()
         self.mask_images = mask_images
+        self.drop_empty_masks()
 
     def check_include_target(self) -> bool:
         """检查是存在mask，存在则返回True"""
         if len(self.mask_images.values()) == 0:
             return False
+        for i in self.mask_images.values():
+            if len(i) > 0:
+                return True
         else:
-            return True
+            return False
 
     def check_boundary(self) -> bool:
         """
@@ -83,7 +115,7 @@ class Patch:
         for mask_type in self.mask_images.keys():
             for mask_image in self.mask_images[mask_type]:
                 x, y, z = mask_image.shape
-                white = tuple([255 for i in range(z)])
+                white = tuple([255 for _ in range(z)])
                 for cur_x in range(x):
                     if (mask_image[cur_x, y] == white or
                             mask_image[cur_x, 0] == white):
@@ -94,10 +126,61 @@ class Patch:
                         return False
         return True
 
-    def apply_to_image_data(self, data: ImageData, pos: tuple = None):
+    def apply_to_image_data(self, data: ImageData, pos: tuple = None) -> ImageData:
+        # 因为需要把新的mask覆盖到旧的上面，所以旧的必须存在
+        assert data.mask_image is not None
+        # TODO:需要解决命名冲突
+        # 需要使用copy来解决引用问题
+        new_data = ImageData(data.name + "_patch" + str(random.randint(10000, 99999)), data.image.copy(),
+                             data.mask_polygons.copy())
         if pos is None:
             # 如果没指定位置，则随机取点，取的点要保证能放下一个patch
-            pos = (randint(0, data.shape[0] - self.shape[0]), randint(0, data.shape[1] - self.shape[1]))
+            pos = (randint(0, new_data.shape[0] - self.shape[0]), randint(0, new_data.shape[1] - self.shape[1]))
         # 把patch的图片覆盖到原图指定位置上
-        data.image[pos[0]:pos[0] + self.shape[0], pos[1]:pos[1] + self.shape[1], :] = self.image
-        # TODO:把mask也贴到原图上
+        new_data.image[pos[0]:pos[0] + self.shape[0], pos[1]:pos[1] + self.shape[1], :] = self.image
+        new_data.convert_polygons_to_images()
+        # 把mask从小的变换到大坐标系中
+        empty_mask = numpy.zeros((data.image.shape[0], data.image.shape[1]), dtype="uint8")
+        for mask_type in self.mask_images.keys():
+            for mask_index in range(len(self.mask_images[mask_type])):
+                cur_big_mask = empty_mask.copy()
+                cur_big_mask[pos[0]:pos[0] + self.shape[0], pos[1]:pos[1] + self.shape[1]] = \
+                    self.mask_images[mask_type][mask_index]
+                self.mask_images[mask_type][mask_index] = cur_big_mask
+        # 把mask也贴到原图上
+        for mask_type in self.mask_images.keys():
+            if mask_type not in new_data.mask_image.keys():
+                new_data.mask_image[mask_type] = self.mask_images[mask_type]
+            else:
+                new_data.mask_image[mask_type] += self.mask_images[mask_type]
+        return new_data
+
+    def drop_empty_masks(self):
+        """去掉空的mask"""
+        # TODO: Drop之后总是清除了所有的mask，需要解决
+        empty = numpy.zeros(self.shape, dtype="uint8")
+        for mask_type in self.types:
+            self.mask_images[mask_type] = [mask for mask in self.mask_images[mask_type] if not (mask == empty).all]
+
+
+def split_img(image: numpy.ndarray, patch_size: tuple) -> list:
+    patches = []
+    for x in range(0, image.shape[0], patch_size[0]):
+        for y in range(0, image.shape[1], patch_size[1]):
+            patches.append(image[
+                           x:x + patch_size[0],
+                           y:y + patch_size[1],
+                           :
+                           ])
+    return patches
+
+
+def split_mask(mask: numpy.ndarray, patch_size: tuple) -> list:
+    patches = []
+    for x in range(0, mask.shape[0], patch_size[0]):
+        for y in range(0, mask.shape[1], patch_size[1]):
+            patches.append(mask[
+                           x:x + patch_size[0],
+                           y:y + patch_size[1]
+                           ])
+    return patches
--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import argparse
 import os
 import random
 import re
+from typing import List
 
 from DataAug import aug_data
-from DataObj import ImageData
+from DataObj import ImageData, Patch
 
 VAL_RATE = 1 / 8
 AUG = False
 PATCH = True
+PATCH_SIZE = (64, 128)
+PATCH_AMOUNT = 1
 
 # 通过命令行参数得到数据源路径
 parser = argparse.ArgumentParser()
@@ -24,8 +27,6 @@ file: str
 picFiles = [file for file in files if re.fullmatch(r".*\.(jpg|png)$", file, re.I)]
 print("读取所有源图片成功：")
 print("\n".join(picFiles))
-if int(len(picFiles)*VAL_RATE) < 1:
-    print("样本数量不足，或者VAL_RATE设置太小(该提示不会影响程序运行)")
 
 # 创建目标输出文件夹
 try:
@@ -37,6 +38,8 @@ except FileExistsError:
 data_file_list = []
 for i in picFiles:
     data_file_list.append(ImageData.create_from_file(i, DataSource))
+for i in data_file_list:
+    i.convert_polygons_to_images()
 
 if AUG:
     print("开始进行图像处理数据增强")
@@ -47,11 +50,32 @@ if AUG:
 if PATCH:
     print("开始进行贴图数据增强")
     AUG_list = []
-    # TODO:把贴图数据增强做好
+    patches = []
+    cur_patches: List[Patch]
+    # 初始化patches
+    for data_file in data_file_list:
+        cur_patches = Patch.create_from_image_data(data_file, patch_size=PATCH_SIZE)
+        for patch in cur_patches:
+            # 如果未包含物体或者遇到了边界，那么抛弃
+            if not patch.check_include_target() or not patch.check_boundary():
+                # if not patch.check_boundary():
+                continue
+            patches.append(patch)
+    for data_file in data_file_list:
+        if len(patches) < PATCH_AMOUNT:
+            print("有效Patch数量小于PATCH_AMOUNT，无法执行该项数据增强")
+            break
+        cur_patches = random.sample(patches, PATCH_AMOUNT)
+        for i in cur_patches:
+            tmp = i.apply_to_image_data(data_file)
+            AUG_list.append(tmp)
+    data_file_list += AUG_list
+
+if int(len(data_file_list) * VAL_RATE) < 1:
+    print("样本数量不足，或者VAL_RATE设置太小(该提示不会影响程序运行)")
 
 for i in data_file_list:
     print(f"正在转换文件:\n{str(i)}")
-    i.convert_polygons_to_images()
     i.dump_masks_and_image(DataTarget)
 
 print(f"转换全部成功，接下来进行随机选择VAL，你的VAL_RATE为{VAL_RATE}")
